d0b97ae04f31cac5c8034ef57db528d63917c99e
Yuliia Garreu <yuliia.garreau@gmail.com>
2025-03-23 14:33:41 +0000

    fix issue empty array

--- src/utils/readProduct.js
+++ src/utils/readProduct.js
@@ -3,5 +3,9 @@ import { PATH_DB } from '../constants/products.js';
 
 export const readProduct = async () => {
   const data = await fs.readFile(PATH_DB, 'utf-8');
-  return JSON.parse(data);
+  const parsedData = data ? JSON.parse(data) : [];
+  console.log('log for data to fix empty array', typeof data);
+  console.log('log for parsedData', typeof parsedData);
+
+  return parsedData;
 };
